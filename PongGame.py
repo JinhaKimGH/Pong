fca4f5808ae28df3f888a41f113a8a2ca332b34c
jinhakim-python <56976518+jinhakim-python@users.noreply.github.com>
2019-11-01 13:17:47 -0400

    Add files via upload

---
+++ PongGame.py
@@ -0,0 +1,182 @@
+import pygame
+
+pygame.init()
+
+#Colour
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (124, 252, 0)
+BLUE = (0, 191, 255)
+ORANGE = (255, 140, 0)
+
+# Screen
+src_width = src_height = 600
+
+screen = pygame.display.set_mode((src_width, src_height))
+pygame.display.set_caption('Pong')
+score_player_one = 0
+score_player_two = 0
+screen.fill(BLACK)
+
+#Positional Variables
+
+paddle_x = 0
+paddle_y = 300
+second_paddle_x = 596
+second_paddle_y = 300
+paddle_dx = paddle_dy = 5
+ball_x = 200
+ball_y = 300
+ball_dx = 0
+ball_dy = 0
+
+#Miscellaneous Variables
+colour = BLUE
+radius = 10
+width = 4
+height = 100
+winner = "blue"
+writecolor = WHITE
+retrycolor = WHITE
+
+#ScoreBoard
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+
+
+run = True
+while run:
+    screen.fill(BLACK)
+    pygame.display.update(pygame.draw.circle(screen, colour, (ball_x, ball_y), radius, radius-1))
+    pygame.display.update(pygame.draw.rect(screen, BLUE, (paddle_x, paddle_y, width, height)))
+    pygame.display.update(pygame.draw.rect(screen, ORANGE, (second_paddle_x, second_paddle_y, width, height)))
+    key = pygame.key.get_pressed()
+    pos = pygame.mouse.get_pos()
+    text = font.render(winner, True, WHITE, BLACK)
+    textRect = text.get_rect()
+    textRect.center = (300, 300)
+    write = font.render('Quit?', True, writecolor, BLACK)
+    writeRect = write.get_rect()
+    writeRect.center = (400, 500)
+    retry = font.render('Retry?', True, retrycolor, BLACK)
+    retryRect = retry.get_rect()
+    retryRect.center = (200, 500)
+    start = font.render('Press Space to Start', True, WHITE, BLACK)
+    startRect = start.get_rect()
+    startRect.center = (300, 550)
+    click = pygame.mouse.get_pressed()
+    retrycolor = WHITE
+    writecolor = WHITE
+    if ball_dx == 0 and ball_dy == 0:
+        pygame.display.update(screen.blit(start, startRect))
+
+
+    if key[pygame.K_SPACE]:
+        ball_dx = 5
+        ball_dy = 5
+
+    if -paddle_dx < paddle_y < 500 + paddle_dx:
+        if key[pygame.K_w]:
+            paddle_y -= paddle_dy
+
+        if key[pygame.K_s]:
+            paddle_y += paddle_dy
+
+    if -paddle_dx == paddle_y:
+        paddle_y = 0
+    if paddle_y == (src_height - height) + paddle_dx:
+        paddle_y = 500
+
+    if -paddle_dx < second_paddle_y < 500 + paddle_dx:
+        if key[pygame.K_UP]:
+            second_paddle_y -= paddle_dy
+
+        if key[pygame.K_DOWN]:
+            second_paddle_y += paddle_dy
+
+    if -paddle_dx == second_paddle_y:
+        second_paddle_y = 0
+    if second_paddle_y == (src_height - height) + paddle_dx:
+        second_paddle_y = 500
+
+    ball_x += ball_dx
+    ball_y += ball_dy
+    if ball_y < 0 + radius or ball_y > 600 - radius:
+        ball_dy *= -1
+    if ball_x == 600:
+        score_player_one += 1
+        ball_x = 200
+        ball_y = 300
+        ball_dx *= -1
+
+    if ball_x == 0:
+        score_player_two += 1
+        ball_x = 200
+        ball_y = 300
+        ball_dx *= -1
+
+    if ball_x - radius == 585 and second_paddle_y <= ball_y - radius <= second_paddle_y + height:
+        if second_paddle_y <= ball_y < second_paddle_y + (height//2):
+            ball_dy = abs(ball_dx)
+            ball_dy *= -1
+            ball_dx *= -1
+            colour = ORANGE
+        if second_paddle_y + (height//2) < ball_y <= second_paddle_y + height:
+            ball_dy = abs(ball_dx)
+            ball_dx *= -1
+            colour = ORANGE
+        if ball_y == second_paddle_y + (height//2):
+            ball_dy *= -1
+            ball_dx *= -1
+            colour = ORANGE
+
+    if ball_x - radius == 5 and paddle_y <= ball_y - radius <= paddle_y + height:
+        if paddle_y <= ball_y < paddle_y + (height // 2):
+            ball_dy = abs(ball_dx)
+            ball_dy *= -1
+            ball_dx *= -1
+            colour = BLUE
+        if paddle_y + (height // 2) < ball_y <= paddle_y + height:
+            ball_dy = abs(ball_dx)
+            ball_dx *= -1
+            colour = BLUE
+        if ball_y == paddle_y + (height // 2):
+            ball_dy *= -1
+            ball_dx *= -1
+            colour = BLUE
+
+    if score_player_one > score_player_two:
+        winner = "Blue Won the Game"
+
+    if score_player_two > score_player_one:
+        winner = "Orange Won the Game"
+
+    if score_player_one == 10 or score_player_two == 10:
+        ball_dx = 0
+        ball_dy = 0
+        pygame.display.update(screen.fill(BLACK))
+        pygame.display.update(screen.blit(text, textRect))
+        pygame.display.update(screen.blit(write, writeRect))
+        pygame.display.update(screen.blit(retry, retryRect))
+
+        if 145 <= pos[0] <= 255 and 485 <= pos[1] <= 515:
+            retrycolor = BLUE
+            if click[0] == 1:
+                score_player_one = 0
+                score_player_two = 0
+                
+
+
+        if 355 <= pos[0] <= 445 and 485 <= pos[1] <= 515:
+            writecolor = ORANGE
+            if click[0] == 1:
+                run = False
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+    screen.fill(BLACK)
+pygame.quit()
+quit()
